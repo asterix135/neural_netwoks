e7d9ce8a2e2c6587a098ee2cf02af54d55a0431c
asterix135 <chgraham@gmail.com>
2016-01-10 22:26:16 -0500

    some progress on plotting

--- misc_projects/simple_network.py
+++ misc_projects/simple_network.py
@@ -3,10 +3,13 @@ Implementation of simple neural network from Week 1 lectures
 """
 import os
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
-SMALL_FILE_PATH = '~/Documents/School/Ryerson_program/CMTH642/Assignment2'
-BIG_FILE_PATH = '~/Documents/Code/kaggle/handwriting'
+SMALL_FILE_PATH = '/Users/christophergraham/Documents/School/Ryerson_program/CMTH642/CMTH642_Assignments/Assignment2'
+BIG_FILE_PATH = '/Users/christophergraham/Documents/Code/kaggle/handwriting'
 SMALL_TEST = 'pendigits.tes.csv'
 SMALL_TRAIN = 'pendigits.tra.csv'
 BIG_TEST = 'test.csv'
@@ -48,6 +51,16 @@ def show_digit():
     pass
 
 
+def plot_mnist_digit(image):
+    """ Plot a single MNIST image."""
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.matshow(image, cmap = matplotlib.cm.binary)
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    plt.show()
+
+
 def analyze_pictures():
     """
     Main wrapper routine
@@ -55,4 +68,16 @@ def analyze_pictures():
     """
     pass
 
+
+curr_dir = os.getcwd()
+print(curr_dir)
+os.chdir(SMALL_FILE_PATH)
+# load training data
+test_set = pd.read_csv(SMALL_TRAIN, header=None)
+#select one image to test print
+test_image = test_set.iloc[[0]].drop(16, axis=1).as_matrix()
+test_image.shape=(4,4)
+print(test_image)
+imgplot = plt.imshow(test_image)
+print(imgplot)
 
