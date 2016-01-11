d654d1cdbc84a12fd9d7c63d81d77bd7bbcba1d7
asterix135 <chgraham@gmail.com>
2016-01-10 20:49:03 -0500

    first commit

---
+++ misc_projects/simple_network.py
@@ -0,0 +1,41 @@
+"""
+Implementation of simple neural network from Week 1 lectures
+"""
+import os
+import numpy as np
+
+
+SMALL_FILE_PATH = '~/Documents/School/Ryerson_program/CMTH642/Assignment2'
+BIG_FILE_PATH = '~/Documents/Code/kaggle/handwriting'
+SMALL_TEST = 'pendigits.tes.csv'
+SMALL_TRAIN = 'pendigits.tra.csv'
+BIG_TEST = 'test.csv'
+BIG_TRAIN = 'train.csv'
+
+
+class SimpleNetwork:
+    """
+    Class to keep and update layer information on pixel
+    """
+    def __init__(self, width, length):
+        self._width = width
+        self._length = length
+        # TODO: initialize array of random values matching image size
+
+
+def load_big_images():
+    pass
+
+
+def load_small_images():
+    pass
+
+
+def show_digit():
+    pass
+
+
+def analyze_pictures():
+    pass
+
+
